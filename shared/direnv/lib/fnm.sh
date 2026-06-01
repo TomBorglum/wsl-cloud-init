cb83cc6cccb80b43ae6f458f446017acae614754
TomBorglum <TomBorglum@users.noreply.github.com>
2026-06-01 15:22:49 +0200

    Create fnm.sh

---
+++ shared/direnv/lib/fnm.sh
@@ -0,0 +1,11 @@
+use_fnm() {
+  local version=$1
+  if [ -n "$version" ]; then
+    eval "$(fnm env --shell bash)"
+    fnm list | grep -q "v${version}" || fnm install "$version"
+    fnm use "$version"
+    local full_version
+    full_version=$(fnm current)
+    PATH_add "$HOME/.fnm/node-versions/${full_version}/installation/bin"
+  fi
+}
