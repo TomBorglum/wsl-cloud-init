79d3bd93d7670d33b22b709e53d0e7721279b59f
Tom Borglum <tom.borglum@gmail.com>
2026-06-12 15:50:04 +0200

    Changed to use dedicated test fixture repository

--- distros/ubuntu/24.04/tests/03-git.sh
+++ distros/ubuntu/24.04/tests/03-git.sh
@@ -13,22 +13,25 @@ for f in /usr/local/share/zsh/site-functions/*.zsh(N); do source "$f"; done
 
 # --- gclone ---
 
-gclone TomBorglum/wsl-cloud-init
+gclone TomBorglum/wsl-test-fixture
 
-if [[ -d ~/projects/wsl-cloud-init ]]; then
-  pass "gclone: ~/projects/wsl-cloud-init was created"
+if [[ -d ~/projects/wsl-test-fixture ]]; then
+  pass "gclone: ~/projects/wsl-test-fixture was created"
 else
-  fail "gclone: ~/projects/wsl-cloud-init was not created"
+  fail "gclone: ~/projects/wsl-test-fixture was not created"
 fi
 
-if [[ -d ~/projects/wsl-cloud-init/.git ]]; then
+if [[ -d ~/projects/wsl-test-fixture/.git ]]; then
   pass "gclone: cloned directory is a git repo"
 else
   fail "gclone: cloned directory is not a git repo"
 fi
 
-if [[ "$PWD" == "$HOME/projects/wsl-cloud-init" ]]; then
+if [[ "$PWD" == "$HOME/projects/wsl-test-fixture" ]]; then
   pass "gclone: cd'd into the cloned directory"
 else
   fail "gclone: did not cd into the cloned directory (PWD=$PWD)"
 fi
+
+# Cleanup
+rm -rf ~/projects/wsl-test-fixture
